d614ff01c69e00d775d34dec891f06c36e7faf9b
amitlavi7 <amitlavi7@gmail.com>
2019-12-13 14:11:49 +0200

    check

---
+++ src/test/java/bgu/spl/mics/InventoryTest.java
@@ -0,0 +1,25 @@
+package bgu.spl.mics;
+
+import main.java.bgu.spl.mics.application.passiveObjects.Inventory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class InventoryTest {
+    private Inventory inv;
+    private String[] gadgets;
+
+    @BeforeEach
+    public void setUp(){
+        inv = Inventory.getInstance();
+        gadgets = new String[]{"sky hook", "space knife", "camera pen", "poisoned shoes"};
+        inv.load(gadgets);
+    }
+
+    @Test
+    public void test(){
+        assertEquals(true, inv.getItem("sky hook"));
+    }
+}
