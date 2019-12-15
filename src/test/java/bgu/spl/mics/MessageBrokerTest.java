e42545f3630f9e8f159ab76ddfaf21a79e6623cb
amitlavi7 <amitlavi7@gmail.com>
2019-12-15 09:45:15 +0200

    check

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -1,14 +1,25 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.example.messages.ExampleBroadcast;
+import bgu.spl.mics.example.messages.ExampleEvent;
+import bgu.spl.mics.example.publishers.ExampleMessageSender;
+import bgu.spl.mics.example.subscribers.ExampleBroadcastSubscriber;
+import bgu.spl.mics.example.subscribers.ExampleEventHandlerSubscriber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageBrokerTest {
+    private MessageBroker broker;
+    private ExampleBroadcast exampleBroadcast;
+    private ExampleEvent exampleEvent;
+    private ExampleMessageSender exampleMessageSender;
+    private ExampleBroadcastSubscriber exampleBroadcastSubscriber;
+    private ExampleEventHandlerSubscriber exampleEventHandlerSubscriber;
     @BeforeEach
     public void setUp(){
-
+        broker = MessageBrokerImpl.getInstance();
     }
 
     @Test
