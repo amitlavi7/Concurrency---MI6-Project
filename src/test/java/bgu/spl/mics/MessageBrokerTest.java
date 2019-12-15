28c699d8d6bd9e637471e89be9dc0ec634804c1d
amitlavi7 <amitlavi7@gmail.com>
2019-12-15 12:06:03 +0200

    007

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -5,15 +5,15 @@ import bgu.spl.mics.example.messages.ExampleEvent;
 import bgu.spl.mics.example.publishers.ExampleMessageSender;
 import bgu.spl.mics.example.subscribers.ExampleBroadcastSubscriber;
 import bgu.spl.mics.example.subscribers.ExampleEventHandlerSubscriber;
-import bgu.spl.mics.application.subscribers.Q;
-import bgu.spl.mics.application.subscribers.Moneypenny;
-import bgu.spl.mics.application.subscribers.M;
-import bgu.spl.mics.application.publishers.Intelligence;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import bgu.spl.mics.application.subscribers.M;
+import bgu.spl.mics.application.subscribers.Q;
+import bgu.spl.mics.application.subscribers.Moneypenny;
+import bgu.spl.mics.application.publishers.Intelligence;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageBrokerTest {
     private MessageBroker broker;
@@ -28,11 +28,13 @@ public class MessageBrokerTest {
         broker = new MessageBrokerImpl();
         exampleEvent = new ExampleEvent("Amit");
         exampleEventHandlerSubscriber = new ExampleEventHandlerSubscriber("Gili", array);
+        exampleBroadcast = new ExampleBroadcast("refael");
+        exampleBroadcastSubscriber = new ExampleBroadcastSubscriber("falafel", array);
+
     }
 
     @Test
     public void test(){
-        //fuck you
         Subscriber q = new Q();
         Subscriber m = new M();
         Subscriber moneypenny = new Moneypenny();
@@ -43,6 +45,7 @@ public class MessageBrokerTest {
         broker.register(moneypenny);
         broker.subscribeEvent(exampleEvent.getClass(), exampleEventHandlerSubscriber);
         assertNotNull(broker.sendEvent(exampleEvent));
+        broker.subscribeBroadcast(exampleBroadcast.getClass(),exampleBroadcastSubscriber);
 
     }
 }
