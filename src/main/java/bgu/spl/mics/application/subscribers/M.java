a6249d77121a00a44cf6c50d9138a81312ba92bc
amitlavi7 <amitlavi7@gmail.com>
2019-12-19 22:44:06 +0200

    amit tafsik lehaziz oti

--- src/main/java/bgu/spl/mics/application/subscribers/M.java
+++ src/main/java/bgu/spl/mics/application/subscribers/M.java
@@ -3,10 +3,7 @@ package bgu.spl.mics.application.subscribers;
 import bgu.spl.mics.Callback;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.Subscriber;
-import bgu.spl.mics.application.messages.AgentsAvailableEvent;
-import bgu.spl.mics.application.messages.GadgetAvailableEvent;
-import bgu.spl.mics.application.messages.MissionReceivedEvent;
-import bgu.spl.mics.application.messages.SendAgentsEvent;
+import bgu.spl.mics.application.messages.*;
 
 /**
  * M handles ReadyEvent - fills a report and sends agents to mission.
@@ -24,15 +21,17 @@ public class M extends Subscriber {
 
 	@Override
 	protected void initialize() {
-		subscribeEvent(MissionReceivedEvent.class, (MissionReceivedEvent event) -> {
+		subscribeEvent(MissionReceivedEvent.class, event -> {
 			System.out.println("M's MissionReceivedEvent callback");//harta
 			Future agentsResolved = getSimplePublisher().sendEvent(new AgentsAvailableEvent(event.getMissionInfo().getSerialAgentsNumbers()));
 			if(agentsResolved.get() == "agentsSucceed") {
 				Future gadgetResolved = getSimplePublisher().sendEvent(new GadgetAvailableEvent(event.getMissionInfo().getGadget()));
 				if (gadgetResolved.get() == "gadgetSucceed") {
 					//need to check time
-					//if statment for time
+					//if statment for time {
 						getSimplePublisher().sendEvent(new SendAgentsEvent(event.getMissionInfo().getSerialAgentsNumbers(),event.getMissionInfo().getDuration()));
+						//}
+					getSimplePublisher().sendEvent(new ReleaseAgentsEvent(event.getMissionInfo().getSerialAgentsNumbers()));
 				}
 			}
 		});
